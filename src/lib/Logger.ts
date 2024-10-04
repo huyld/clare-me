6bc4d41fa7fbd633777a9bbf7d11a0c83098868b
Huy Luu <3625371+huyld@users.noreply.github.com>
2024-10-04 20:02:34 +0200

    feat(logger): config logger

---
+++ src/lib/Logger.ts
@@ -0,0 +1,28 @@
+import { createLogger, transports, format } from 'winston'
+
+const level = process.env.NODE_ENV === 'development' ? 'debug' : 'warn'
+
+const logger = createLogger({
+  transports: [
+    new transports.Console({
+      level: level,
+      format: format.combine(
+        format.errors({ stack: true }),
+        format.prettyPrint(),
+      ),
+    }),
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+})
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: format.simple(),
+  }))
+}
+
+export default logger
