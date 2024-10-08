444f42134629fac117a6ba48608c82d1345aa1db
Huy Luu <3625371+huyld@users.noreply.github.com>
2024-10-09 01:26:06 +0200

    feat(api): added POST /sendMessage endpoint

--- src/api/conversation.ts
+++ src/api/conversation.ts
@@ -9,5 +9,15 @@ router.get(
   },
 )
 
+router.post(
+  '/sendMessage',
+  async (req, res) => {
+    // TODO: Classify messages and handle flows.
+    const message = req.body
+
+    res.send('This endpoint is still under construction.')
+  },
+)
+
 export default router
 
