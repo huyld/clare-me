b611217957a3b45e74c3f95b92673cc65c4403aa
Huy Luu <3625371+huyld@users.noreply.github.com>
2024-10-09 01:24:55 +0200

    feat(api): setup middleware body-parser

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,13 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import conversation from './conversation'
 
 const router = express.Router()
+
+const jsonMiddleware = bodyParser.json({
+  limit: '200kb',
+})
+router.use(jsonMiddleware)
 router.use('/', conversation)
 
 export default router
