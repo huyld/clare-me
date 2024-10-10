56c0249a6dabd467d7e27a18274bc44aad97709a
Huy Luu <3625371+huyld@users.noreply.github.com>
2024-10-10 21:05:36 +0200

    feat(api): save message to store for POST /sendMessage

--- src/api/conversation.ts
+++ src/api/conversation.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import { v4 as uuid } from 'uuid'
 import AuthService from '../services/auth'
+import Message from '../models/Message'
+import Stores from '../store/types/Stores'
 
 const router = express.Router()
 
-function init(authService: AuthService) {
+function init(authService: AuthService, stores: Stores) {
   router.get(
     '/',
     async (req, res) => {
@@ -17,6 +20,14 @@ function init(authService: AuthService) {
     async (req, res) => {
       // TODO: Classify messages and handle flows.
       const body = req.body
+      const message = {
+        id: uuid(),
+        from: body.from,
+        to: 'clare',
+        text: body.text,
+        createdAt: Date.now(),
+      } as Message
+      await stores.conversationStore.save(message)
 
       res.send('This endpoint is still under construction.')
     },
--- src/api/index.ts
+++ src/api/index.ts
@@ -2,15 +2,17 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import conversation from './conversation'
 import AuthService from '../services/auth'
+import Stores from '../store/types/Stores'
 
 const router = express.Router()
 
 const jsonMiddleware = bodyParser.json({
   limit: '200kb',
 })
-function init(authService: AuthService) {
+
+function init(authService: AuthService, stores: Stores) {
   router.use(jsonMiddleware)
-  router.use('/', conversation(authService))
+  router.use('/', conversation(authService, stores))
   return router
 }
 
