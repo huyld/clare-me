839ba70a3d6dcf972c1180dde54d1f5a489548a6
Huy Luu <3625371+huyld@users.noreply.github.com>
2024-10-11 21:07:46 +0200

    feat(lib): added questions and answers from FAQ to constant file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,26 +1,58 @@
-const FAQ_LIST = [
-  "How do I get started with the product?",
-  "Can Clare help me with emotional problems, such as anxiety?",
-  "Is this anonymous?",
-  "Can I call anytime? ",
-  "It's my first time with a virtual assistant, how should I behave? ",
-  "Does Clare offer psychotherapy?",
-  "Is Clare the right solution for me?",
-  "Can talking to Clare replace my traditional therapy? ",
-  "Why does clare&me not provide support for suicide and suicidal tendencies?",
-  "Where can I leave a review?",
-  "What is clare&me?",
-  "Is Clare a human? ",
-  "How does it work?",
-  "What can Clare do?",
-  "Can I speak to Clare as I would with a human being? ",
-  "Are there any useful commands Clare understands, that will improve our communication? ",
-  "What are the costs of talking to Clare?",
-  "How can I cancel my subscription?",
-  "How can I edit my payment details? I used a PayPal account.",
-  "How can I edit my payment details? I don't have a PayPal account.",
-]
+const FAQ = {
+  "How do I get started with the product?": "Simply click on one of the buttons on the Home Page and send the pre-filled message ‘Start 7-day trial’ via WhatsApp. Clare will guide you through the whole process via chat and you’ll have a chance for the first introduction call. Note that you’ll need to define a calling routine. Most people experience the greatest impact from Clare if they schedule phone calls once a week. This has to do with continuity and practice. Find a time that works for you and try to stick with it.",
+  "Can Clare help me with emotional problems, such as anxiety?": "Clare provides information so that you can learn to be your own “expert” - understanding yourself is the first step in self-help. Clare will support you as you (re)discover beneficial and self-helping techniques. Clare provides you with short-term and long-term coping strategies for emotional problems. However, self-help has limits. If you are struggling continually and/or with strong symptoms of anxiety, we recommend that you reach out to a professional psychotherapist close to you.",
+  "Is this anonymous?": "Yes. Although we ask a few questions about you during sign-up (first name, age), you use the product anonymously and without any human contact. For security reasons, we are obliged to review anonymized transcripts or recordings of calls and conversations to ensure you and Clare stay safe.",
+  "Can I call anytime?": `If you are on the free plan you can call and text Clare anytime between 9 am and 5 pm in your local time.
+The the premium version you can call and text Clare 24/7 every day.
+Send Clare a WhatsApp message saying ‘call me now’ and you’ll receive a call within a few seconds.`,
+  "It's my first time with a virtual assistant, how should I behave?": "Clare will guide you through questions, allowing you to open up and share your thoughts. You will sometimes be given different prompts and should you choose to stay quiet during the call, that’s also fine. It’s normal to feel insecure at first. Give it a try and you’ll notice how it gets easier to talk to Clare with time.",
+  "Does Clare offer psychotherapy?": "No. Clare does not offer traditional psychotherapy. Clare is not human but is a voice bot designed to guide you through your self-help journey. Clare supports you in taking time for yourself, be it to speak your mind, reflect, or increase awareness. You will be offered exercises to reflect on your behavior, thoughts, and feelings. This is the first step toward behavioral change. Clare will also provide you with various tools you can practice to cope with daily hassles.",
+  "Is Clare the right solution for me?": `This is something you can only find out by trying it. We believe Clare allows for an innovative way to reflect, discover, and cope. This is done by talking to Clare on the phone. The onboarding call can give you a good sense of how and whether it works for you.
+Clare is an empathetic and non-judgemental listener, allowing yourself to take a curious look at your own beliefs, emotions, and behavior. If you want to do this, Clare is right for you! Talking to Clare can incite emotions such as joy and surprise, but maybe also anxiety or anger. If you feel that Clare makes you feel uncomfortable, please be easy on yourself and leave us a comment.`,
+  "Can talking to Clare replace my traditional therapy?": "There is a clear difference between humans and artificial intelligence. Clare does not aim to be a replacement for traditional psychotherapy. If you have a psychotherapist already, it may make sense to talk about and reflect upon your experience with Clare.",
+  "Why does clare&me not provide support for suicide and suicidal tendencies?": `Suicidal thoughts can be concerning and we take this very seriously. We have a duty of care. This means that we have to inform you of the limitations of Clare.
+Clare does not have the experience yet to deal with complex issues, such as self-harm and suicidal thoughts. We want to make sure that you are given the help you need right now. This is why we strongly recommend that you reach out to a human being, be it as a good friend or family member, or a practitioner (such as a therapist or your GP) so that you have face-to-face contact. There are various professional support services available for people with mental health problems and suicidal tendencies. We have compiled an overview of the most important services on our Crisis Page.
+There is no shame in asking for help, having someone listen and be there for you.`,
+  "Where can I leave a review?": "You can write us the feedback via Contact Form located on our website, or directly to hello@clareandme.com. As always, we welcome your feedback, suggestions and comments.",
+  "What is clare&me?": `Clare is an artificial intelligence that supports and guides you through your mental health journey.  
+Clare’s approach is conversational and resource-oriented. Clare draws on various resources: psychoeducation and insights stem from scientific research; practical tools and techniques are inspired by self-compassion, stress-management, positive psychology, resilience, and meditation practices. The content Clare provides is curated by psychologists and overseen by our advisors.`,
+  "Is Clare a human?": "No, Clare is not human. They are a bot powered by artificial intelligence. Clare speaks to you, responds to what you're saying and can remember things you say. Hence, speaking to them might differ from a human interaction: pauses might be a little longer, there are sensitivities to noise or misunderstandings can occur. Please be mindful for what helps you in a specific situation, it might be speaking to a human friend instead of a bot.",
+  "How does it work?": "You can speak to Clare via phone calls and WhatsApp messaging. You can stick to your weekly schedule and only answer Clare’s incoming calls, or trigger a call at the time that suits you best, anytime and anywhere. The product is still developing, so Clare is still learning how to talk to you better.",
+  "What can Clare do?": "Clare is available to talk to you anytime and anywhere, always being there for you via calls and/or WhatsApp. By talking to Clare, you can be guided through reflection, find assistance in processing your emotions, and be given guidance on your mental health journey.",
+  "Can I speak to Clare as I would with a human being?": "You can speak to Clare as you would do it with a human being. Clare is designed to have human-like interactions and to understand what you’re saying. There is no need to talk slower or shorter. We recommend finding a quiet space, as Clare might have trouble differentiating between your voice and background voices. As Clare is still in beta, please be patient in case they require some longer pauses in order to properly match your conversation. Clare will make sure you’re done talking before they continue. Also, Clare is designed for mental health-related conversations and unrelated topics will lead to a confusing conversation. Please be patient and let us know what went wrong so that we can improve faster.",
+  "Are there any useful commands Clare understands, that will improve our communication?": `Yes, there are a few recommendations. Remember: you can answer freely, but to make it easier for you, here’s what Clare has internalized:
+On WhatsApp:
+- Call me now - receiving the call within a few seconds
+- What’s my schedule? - requesting your weekly call schedule
+- When’s my next call? - requesting the date and time of your next call
+- Don’t call me today - canceling your scheduled call of the day
+- Update my schedule - updating your schedule to fit your routine
+- Can we text? - supporting you via text by helping you reflect, doing exercises with you or sending you mindfulness meditation exercises via voice notes
+- I can’t sleep, I feel frustrated, I worry a lot, I feel anxious, I’m self-critical - receiving support matching your specific feeling or topic of choice (more to come, keep texting)
+- Cancel calls - pause scheduled calls (do not cancel the subscription and payment)
+- Cancel messages - pause messages (do not cancel the subscription and payment)
+- Cancel the subscription - starting a cancelation of the subscription
+- Turn calls back on -  resume scheduled calls
+On calls:
+- Asking “Can you repeat? “-  Clare will repeat what they said
+- Staying silent - Clare accommodates you after you do an exercise in your head`,
+  "What are the costs of talking to Clare?": `You can talk to Clare for free from 9 am to 5 pm daily in your local time.
+Our premium plan offers unlimited messages and calls with Clare 24/7 for  £99.99 annually.
+We offer a 7-day free premium trial. To begin the free trial, you securely enter your payment details through Paypal. Should you decide that Clare is not the right fit for you during these 7 days, simply send a message with “cancel my subscription” to avoid charges.
+Your subscription can be cancelled at any time. After cancellation you’ll be on the free plan and can talk to Clare from 9 am to 5 pm daily.
+Please be aware, should you call Clare back, your phone provider might charge you.If you have any issues with the subscription and payment process, please reach out to us by writing us an email at: support@clareandme.com
+Please note: as a gesture of our appreciation, those who signed up during our development phase until June 27th 2023, will enjoy Clare’s 24/7 availability for free.`,
+  "How can I cancel my subscription?": `Send Clare a message saying 'Cancel my subscription' and Clare will cancel the subscription for you. You will receive the confirmation message on WhatsApp once the cancelation is done.
+If you have any issues with the payment process, please reach out to us by writing us an email at: support@clareandme.com`,
+  "How can I edit my payment details? I used a PayPal account.": `If you subscribed using PayPal account, you can access the subscription and edit it by logging in to the PayPal app or using the PayPal website. Please follow this instruction to edit your payment details.
+If you have any issues with the payment process, please reach out to us by writing us an email at: support@clareandme.com`,
+      "How can I edit my payment details? I don't have a PayPal account.": `If you subscribed without creating PayPal account, but just by providing your Debit or Credit Card, please cancel your subscription by texting Clare 'Cancel subscription'. You'll receive the confirmation of the cancelation after which you can subscribe again by texting 'Subscribe now' and providing the details of the new card via PayPal service. You don't need to create a new account, Clare will remember you.
+If you have any issues with the payment process, please reach out to us by writing us an email at: support@clareandme.com`,
+}
+
+const FAQ_LIST = Object.keys(FAQ)
 
 export {
+  FAQ,
   FAQ_LIST,
 }
